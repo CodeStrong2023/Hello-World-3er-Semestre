f68c2209cb0dd40ce0021752eba1fd73709a1a0b
leoeugenio16 <leoeugenio16@gmail.com>
2024-04-25 07:21:31 -0300

    Clase 3 Funciones
    Trabjando con JS

--- Leandro/JavaScript/semana2/TSLeccion2/02-03-arreglos.js
+++ Leandro/JavaScript/semana2/TSLeccion2/02-03-arreglos.js
@@ -7,7 +7,7 @@ console.log(autos);
 console.log(autos[0]);
 console.log(autos[2]);
 
-for(let i = 0; i < autos.length){
+for (let i = 0; i < autos.length; i++) {
     console.log(i+' : '+autos)
 }
 
