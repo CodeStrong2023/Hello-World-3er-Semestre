dd7268ea395623947d6c45c92a5aae838230b1fc
Hernan Raso <82213611+hernanraso@users.noreply.github.com>
2024-04-27 22:03:45 -0300

    Leccion 2
    Subiendo la clase 2 que es sobre los archivos de la leccion 1

--- Hernan Raso/Python/Leccion01/pythonProject/figuraGeometrica.py
+++ Hernan Raso/Python/Leccion01/pythonProject/figuraGeometrica.py
@@ -1,7 +1,18 @@
-class figuraGeometrica:
+from abc import ABC, abstractmethod
+# ABC significa: Abstract Base Class, convierte una clase en abstracta
+
+class figuraGeometrica(ABC):
     def __init__(self, ancho, alto):
-        self._ancho = ancho
-        self._alto = alto
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            self.ancho = 0
+            print(f'Valor erroneo para el ancho: {ancho}')
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            self.alto = 0
+            print(f'Valor erroneo para el alto: {alto}')
 
     @property
     def ancho(self):
@@ -9,7 +20,10 @@ class figuraGeometrica:
 
     @ancho.setter
     def ancho(self, ancho):
-        self._ancho = ancho
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            print(f'Valor erroneo ancho: {ancho}')
 
     @property
     def alto(self):
@@ -17,8 +31,17 @@ class figuraGeometrica:
 
     @alto.setter
     def alto(self, alto):
-        self._alto = alto
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            print(f'Valor erroneo Alto: {alto}')
+
+    @abstractmethod
+    def calcular_area(self):
+        pass
 
     def __str__(self):
         return f'figuraGeometrica [Ancho: {self._ancho}, Alto: {self._alto}]'
 
+    def _validar_valores(self, valor):   # Metodo encapzulado
+        return True if 0 < valor < 10 else False
