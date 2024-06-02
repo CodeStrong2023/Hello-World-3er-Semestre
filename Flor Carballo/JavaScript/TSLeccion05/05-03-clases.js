f7725499a65ab6080edeb1d97ef1c280acb86fb2
Flor <popiflorencia@yahoo.com.ar>
2024-06-01 23:13:15 -0300

    Clase JavaScript
    Clases de javascript

--- Flor Carballo/JavaScript/TSLeccion05/05-03-clases.js
+++ Flor Carballo/JavaScript/TSLeccion05/05-03-clases.js
@@ -1,9 +1,24 @@
 //let persona3 = new Persona("Facundo", "Ponce"); No funciona, no se puede crear un objeto antes que la clase. Persona no esta definida
 
 class Persona{ //clase padre
+
+    static contadorPersonas = 0; //atributo estático
+    //email = "Valor default email"; //atributo no estático
+
+    static get MAX_OBJ(){ // este metodo simula una constante
+        return 5;
+    }
+
     constructor(nombre, apellido){
         this._nombre = nombre;
         this._apellido = apellido;
+        if(Persona.contadorPersonas < Persona.MAX_OBJ){
+            this.idPersona = ++Persona.contadorOPersonas;
+        }
+        else{
+            console.log("Se ha superado el máximo de objetos permitidos");
+        }
+        //console.log('Se incrementa el contador: '+Persona.contadorObjetosPersona);
     }
 
     get nombre(){
@@ -21,13 +36,21 @@ class Persona{ //clase padre
     }
 
     nombreCompleto(){
-        return this._nombre+" "+this._apellido;
+        return this.idPersona+' '+this._nombre+" "+this._apellido;
     }
     //Sobreescribiendo el metodo de la clase padre (object)
     toString(){ //Regresa un String
         //Se aplica el polimorfismo que significa = multiples formas en tiempo de ejecución
         //El metodo que se ejecuta depende si es una referencia de tipo padre o hija
         return this.nombreCompleto();
+    }
+
+    static saludar(){
+        console.log("Saludos desde este método statics");
+    }
+
+    static saludar2(persona){
+        console.log(persona.nombre+" "+persona.apellido);
     }
 }
 class Empleado extends Persona{ //clase hija
@@ -71,3 +94,33 @@ console.log(empleado1.nombreCompleto());
 console.log(empleado1.toString());
 console.log(persona1.toString());
 
+//persona1.saludar(); no se utiliza desde el objeto
+Persona.saludar();
+Persona.saludar2(persona1);
+
+Empleado.saludar();
+Empleado.saludar2(empleado1);
+
+//console.log(persona1.contadorObjetosPersona);
+console.log(Persona.contadorObjetosPersona);
+console.log(Empleado.contadorObjetosPersona);
+
+console.log(persona1.email);
+console.log(empleado1.email);
+//console.log(Persona.email); No es estático
+console.log(persona1.toString());
+console.log(persona2.toString());
+console.log(empleado1.toString());
+console.log(Persona.contadorPersonas);
+let persona3 = new Persona('Carla', 'Pertosi');
+console.log(persona3.toString());
+console.log(Persona.contadorPersonas);
+
+console.log(Persona.MAX_OBJ);
+//Persona.MAX_OBJ = 10; No se puede modificar, ni alterar
+console.log(Persona.MAX_OBJ);
+
+let persona4 = new Persona('Franco', "Diaz");
+console.log(persona4.toString());
+let persona5 = new Persona("Liliana", "Paz");
+console.log(persona5.toString());
