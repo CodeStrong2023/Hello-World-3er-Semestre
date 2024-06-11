f6210517b8965e9f75719c0481bb994c8b32d650
Mirco Karzovnik <mircoeliank@gmail.com>
2024-06-10 21:20:58 -0300

    Elimino archivos

--- Mirco/Tecnicatura3Java/Leccion05/AbstractClass/src/test/AbstractTest.java
+++
@@ -1,8 +0,0 @@
-package test;
-import domain.*;
-public class AbstractTest {
-    public static void main(String[] args){
-        GeometricFigure figure = new Rectangle("Rectangle");
-        figure.Draw();
-    }
-}
