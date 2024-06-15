f96170513d1597800ae6990e314062cb62b08bb8
Flor <popiflorencia@yahoo.com.ar>
2024-06-15 17:25:11 -0300

    Clase 10 Python
    commit de la Clase 10 Python

--- Flor Carballo/Python/Leccion8/BD/prueba_bd.py
+++ Flor Carballo/Python/Leccion8/BD/prueba_bd.py
@@ -1,19 +1,16 @@
 import psycopg2 # Esto es para poder conectarnos a Postgre
 
-conexion = psycopg2.connect(
-    user = 'postgres',
-    password = 'admin',
-    host = '127.0.0.1',
-    port = '5432',
-    database = 'test_bd'
-)
-
-cursor = conexion.cursor()
-sentencia = 'SELECT * FROM persona'
-cursor.execute(sentencia) # de esta manera ejecutamos la sentencia
-registros = cursor.fetchall() # recuperamos todos los registros que serán una lista
-print(registros)
-
-cursor.close()
-conexion.close()
+conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT * FROM persona WHERE id_persona = %s' # Placeholder
+            id_persona = input("Digite un número para el id_persona: ")
+            cursor.execute(sentencia, (id_persona,)) # de esta manera ejecutamos la sentencia
+            registros = cursor.fetchone() # recuperamos todos los registros que serán una lista
+            print(registros)
+except Exception as e:
+    print(f"Ocurrió un error: {e}")
+finally:
+    conexion.close()
 
